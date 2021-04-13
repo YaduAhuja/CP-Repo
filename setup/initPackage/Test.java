3c485186a3b59381352eb3a18cd444d8f9c9997a
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-04-13 15:58:50 +0530

    Changed the Input from Scanner to BufferedReader

--- setup/initPackage/Test.java
+++ setup/initPackage/Test.java
@@ -1,11 +1,12 @@
 package setup.initPackage;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Test {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
+	public static void main(String[] args) throws Exception{
+		BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+		String s = sc.readLine();
 		sc.close();
 		System.out.println(s);
 	}
