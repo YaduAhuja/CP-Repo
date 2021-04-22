7b2d4d633f4930299b820421557a660d79d8b181
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-04-22 22:03:44 +0530

    Optimized the Code for cses Apartments Problem

--- cses/sortingAndSearching/Apartments.java
+++ cses/sortingAndSearching/Apartments.java
@@ -1,14 +1,12 @@
 /**
  *  Problem Link : https://cses.fi/problemset/task/1084/
- *  Runtime : 0.91s
+ *  Runtime : 0.60s
  */
 
 package cses.sortingAndSearching;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class Apartments{	
@@ -19,29 +17,77 @@ public class Apartments{
 		int m = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine().trim()," ");
-		ArrayList<Integer> apartments = new ArrayList<>(m);
-		ArrayList<Integer> customers = new ArrayList<>(n);
-		for(int i = 0; i < n; i++)customers.add(Integer.parseInt(st.nextToken()));
+		int apartments[] = new int[m];
+		int customers[] = new int[n];
+		for(int i = 0; i < customers.length; i++)customers[i] = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine().trim()," ");
-		for(int i = 0; i < m; i++) apartments.add(Integer.parseInt(st.nextToken()));
+		for(int i = 0; i < apartments.length; i++) apartments[i] = Integer.parseInt(st.nextToken());
 		br.close();
 		System.out.println(getApplicants(apartments, customers, k));
 	}
 
-	private static int getApplicants(ArrayList<Integer> apartments, ArrayList<Integer> customers, int k){
-		Collections.sort(apartments);
-		Collections.sort(customers);
+	private static int getApplicants(int []apartments, int[] customers, int k){
+		sort(apartments);
+		sort(customers);
 		int count = 0;
 		
-		for(int cptr = 0, aptr = 0; aptr < apartments.size() && cptr < customers.size();){
-			if(apartments.get(aptr) <= customers.get(cptr)+k && apartments.get(aptr) >= customers.get(cptr)-k){
+		for(int cptr = 0, aptr = 0; aptr < apartments.length && cptr < customers.length;){
+			if(apartments[aptr] <= customers[cptr]+k && apartments[aptr] >= customers[cptr]-k){
 				aptr++;
 				cptr++;
 				count++;
-			}else if(customers.get(cptr) < apartments.get(aptr)- k)cptr++;
+			}else if(customers[cptr] < apartments[aptr]- k)cptr++;
 			else aptr++;
 		}
 
 		return count;
 	}
+
+	// Heap Sort For Faster Sorting than Arrays.sort
+	private static void sort(int arr[])
+    {
+        int n = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+ 
+        // One by one extract an element from heap
+        for (int i = n - 1; i > 0; i--) {
+            // Move current root to end
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
+    }
+ 
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    private static void heapify(int arr[], int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
 }
