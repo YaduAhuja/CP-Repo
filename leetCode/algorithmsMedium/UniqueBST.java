4277fee83dd1e4661b150f2aeaf9f2b4d8f1c324
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-04-22 13:59:38 +0530

    Added the LeetCode Package and Solution to Problem Unique BST from algortihms Medium

---
+++ leetCode/algorithmsMedium/UniqueBST.java
@@ -0,0 +1,29 @@
+/**
+ *  Problem Link : https://leetcode.com/problems/unique-binary-search-trees/
+ *  Runtime : 0.00
+ */
+
+package leetCode.algorithmsMedium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class UniqueBST{
+	private static ArrayList<Integer> countList = new ArrayList<>(Arrays.asList(1,1));
+	public static void main(String[] args) {
+		for(int i = 0; i < 20; i++)
+		System.out.println(getUniqueBSTCount(i));
+	}
+
+	private static int getUniqueBSTCount(int n){
+		int count = 0;
+		if(n < countList.size()) return countList.get(n);
+		for(int i = 0; i < Math.ceil((double)n/2); i++){
+			if(i > n/2 - 1)count += getUniqueBSTCount(i)* getUniqueBSTCount(n-(i+1));
+			else count += 2*getUniqueBSTCount(i)* getUniqueBSTCount(n-(i+1));
+		}
+
+		countList.add(count);
+		return countList.get(n);
+	}
+}
