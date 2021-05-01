a9aac05a4fcb6be64907deae8ee4abdb90eb692d
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-05-01 07:17:08 +0530

    Added Solution to Introductory Problem Bit Strings

---
+++ cses/introductoryProblems/BitStrings.java
@@ -0,0 +1,24 @@
+/**
+ *  Problem Link : https://cses.fi/problemset/task/1617
+ *  Runtime : 0.14s
+ */
+package cses.introductoryProblems;
+
+import java.util.Scanner;
+
+public class BitStrings {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(getCombinations(n));
+	}
+
+	private static int getCombinations(int n){
+		if(n < 1) return 0;
+		final int MOD = (int)1e9+7;
+		int ret = 1;
+		for(int i = 0; i < n; i++) ret = (ret << 1) %MOD;
+		return ret;
+	}
+}
