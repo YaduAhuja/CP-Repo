5134a52d66b0d9ba4b68b227714b93b36fbd805d
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-05-01 07:38:56 +0530

    Added Solution to Introductory Problem Trailing Zeros

---
+++ cses/introductoryProblems/TrailingZeros.java
@@ -0,0 +1,24 @@
+/**
+ *  Problem Link : https://cses.fi/problemset/task/1618/
+ *  Runtime : 0.13s
+ */
+
+package cses.introductoryProblems;
+
+import java.util.Scanner;
+
+public class TrailingZeros {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		System.out.println(countTrailingZeros(n));
+	}
+
+	private static int countTrailingZeros(int n){
+		int sum = 0;
+		for(int divisor = 5; divisor <= n; divisor *= 5) sum += n/divisor;
+		return sum;
+	}
+
+}
