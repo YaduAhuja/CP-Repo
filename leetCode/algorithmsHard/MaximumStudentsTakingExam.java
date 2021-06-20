b549aec011f5624c4bb0fb16cebaa4e74ae203e3
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-06-20 17:10:10 +0530

    Added DP Bitmask solution

---
+++ leetCode/algorithmsHard/MaximumStudentsTakingExam.java
@@ -0,0 +1,21 @@
+package leetCode.algorithmsHard;
+
+public class MaximumStudentsTakingExam {
+	public static void main(String[] args) {
+		char seats[][] = {	{'#','.','#','#','.','#'},
+							{'.','#','#','#','#','.'},
+							{'#','.','#','#','.','#'}};
+		
+		System.out.println(maxStudents(seats));
+	}
+
+
+	private static int maxStudents(char [][] seats){
+		return 0;
+	}
+
+
+	
+
+
+}
