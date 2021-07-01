7f9dc6dfeff0a357a09925623ce05878d720468b
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-07-01 21:10:08 +0530

    Addded CodeForces Solutions

---
+++ codeForces/round65/WayTooLongWords.java
@@ -0,0 +1,30 @@
+package codeForces.round65;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class WayTooLongWords {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i < t; i++){
+			sb.append(getString(br.readLine().trim())).append("\n");
+		}
+
+		br.close();
+		System.out.print(sb);
+	}
+
+	private static String getString(String s){
+		if(s.length() > 10) {
+			StringBuilder sb = new StringBuilder();
+			sb.append(s.charAt(0));
+			sb.append(Integer.toString(s.length()-2));
+			sb.append(s.charAt(s.length()-1));
+			return sb.toString();
+		}
+		return s;
+	}
+}
