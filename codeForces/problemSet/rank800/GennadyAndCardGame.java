4f5015a8c0c32d65be6d9c05622174c113f01405
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-07-05 18:42:20 +0530

    Added More solution to Codeforces problemset

---
+++ codeForces/problemSet/rank800/GennadyAndCardGame.java
@@ -0,0 +1,27 @@
+/**
+ * Problem Link : https://codeforces.com/problemset/problem/1097/A
+ * Runtime : 0.187s
+ */
+
+package codeForces.problemSet.rank800;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GennadyAndCardGame {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String inp = br.readLine().trim();
+		String[] cards = br.readLine().trim().split(" ");
+		boolean found = false;
+		for(String card : cards){
+			if(card.charAt(0) == inp.charAt(0) || card.charAt(1) == inp.charAt(1)){
+				found = true;
+			}
+		}
+
+		br.close();
+		if(found) System.out.println("YES");
+		else System.out.println("NO");
+	}
+}
---
+++ codeForces/problemSet/rank800/NastyaIsReadingBook.java
@@ -0,0 +1,33 @@
+/**
+ * Problem Link : https://codeforces.com/problemset/problem/1136/A
+ * Runtime : 0.187s
+ */
+package codeForces.problemSet.rank800;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NastyaIsReadingBook {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine().trim());
+		int book[][] = new int[n][2];
+		for(int i = 0; i < book.length; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine().trim()," ");
+			book[i][0] = Integer.parseInt(st.nextToken());
+			book[i][1] = Integer.parseInt(st.nextToken());
+		}
+		int page = Integer.parseInt(br.readLine().trim());
+		br.close();
+		int i = 0;
+		for(; i < book.length; i++){
+			if(page <= book[i][1]){
+				break;
+			}
+		}
+
+		System.out.println(n-i);
+
+	}
+}
