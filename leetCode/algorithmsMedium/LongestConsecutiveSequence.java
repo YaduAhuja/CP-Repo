f6b4e1de7118405ca21ada4aa22563b9384feea6
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-07-09 17:59:56 +0530

    removed extra comments

--- leetCode/algorithmsMedium/LongestConsecutiveSequence.java
+++ leetCode/algorithmsMedium/LongestConsecutiveSequence.java
@@ -14,10 +14,6 @@ public class LongestConsecutiveSequence {
         };
         // int arr[] = {100,4,200,1,3,2};
         // int arr[] = {0,3,7,2,5,8,4,6,0,1};
-        // Arrays.sort(arr);
-        // System.out.println(Arrays.toString(arr));
-        // System.out.println(new
-        // TreeSet<>().addAll(Arrays.stream(arr).boxed().collect(Collectors.toList())));
         System.out.println(longestConsecutive(arr));
     }
 
