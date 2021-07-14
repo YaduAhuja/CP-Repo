d7eb43f7c69d561b5b39cb74a41f7d0a0839c20d
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-07-14 12:49:21 +0530

    Added More Solutions

---
+++ codeForces/problemSet/rank1000/KDivisibleSum.java
@@ -0,0 +1,32 @@
+/**
+ * Problem Link : https://codeforces.com/problemset/problem/1476/A
+ */
+package codeForces.problemSet.rank1000;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class KDivisibleSum {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0; i < t; i++){
+			StringTokenizer st = new StringTokenizer(br.readLine().trim()," ");
+			int n = Integer.parseInt(st.nextToken());
+			int k = Integer.parseInt(st.nextToken());
+			sb.append(getMaxElement(n,k)).append('\n');
+		}
+		br.close();
+		System.out.print(sb);
+	}
+
+	private static int getMaxElement(int n, int k) {
+		long x = n;
+		long y = k;
+		long cf = (n + k - 1) / k;
+		y *= cf;
+		return  (int)((x + y - 1) / x); 
+	}
+}
---
+++ codeForces/problemSet/rank800/KeanuReeves.java
@@ -0,0 +1,30 @@
+/**
+ *  Problem Link : https://codeforces.com/problemset/problem/1189/A
+ */
+package codeForces.problemSet.rank800;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class KeanuReeves{
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		String s = br.readLine().trim();
+		br.close();
+		
+		int ZCount = 0, OCount = 0;
+		for(int i = 0; i < s.length(); i++) {
+			if(s.charAt(i) == '0') ZCount ++;
+			else OCount ++;
+		}
+
+		if(ZCount != OCount){
+			System.out.println(1);
+			System.out.println(s);
+		}else{
+			System.out.println(2);
+			System.out.println(s.substring(0, s.length()-1) +" "+ s.substring(s.length()-1));
+		}
+	}
+}
