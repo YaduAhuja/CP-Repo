5b972fa81722cefc13407c3ce269a43a5c732a73
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-08-17 18:15:55 +0530

    Added Round 738 Solutions

---
+++ codeForces/round738/MochaAndHiking.java
@@ -0,0 +1,51 @@
+/**
+ *  Problem Link: https://codeforces.com/contest/1559/problem/C
+ *  Runtime: 0.202s
+ */
+
+package codeForces.round738;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MochaAndHiking {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		var sb = new StringBuilder();
+
+		while(t-- > 0){
+			int n = Integer.parseInt(br.readLine().trim());
+			int arr[] = nextIntArray(n, br);
+			sb.append(findPath(arr)).append("\n");
+		}
+		br.close();
+		System.out.println(sb);
+	}
+
+	private static String findPath(int arr[]){
+		int prev = 0;
+		boolean pushed = false;
+		var sb = new StringBuilder();
+
+		for(int i = 0; i < arr.length; i++){
+			if(prev == 0 && arr[i] == 1 && !pushed){
+				sb.append(arr.length+1).append(" ");
+				pushed = true;
+			}
+			sb.append(i+1).append(" ");
+			prev = arr[i];
+		}
+		if(!pushed) sb.append(arr.length+1);
+		return sb.toString();
+	}
+
+	private static int[] nextIntArray(int N, BufferedReader br) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine().trim()," ");
+		int arr[] = new int[N];
+		for(int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		return arr;
+	}
+}
---
+++ codeForces/round738/MochaAndMath.java
@@ -0,0 +1,38 @@
+/**
+ *  Problem Link : https://codeforces.com/contest/1559/problem/A
+ *  Runtime : 0.202s
+ */
+
+package codeForces.round738;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MochaAndMath {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine().trim());
+		var sb = new StringBuilder();
+
+		while(t-- > 0){
+			int n = Integer.parseInt(br.readLine().trim());
+			int arr[] = nextIntArray(n, br);
+			int ret = arr[0];
+			for(int i = 1; i < arr.length; i++) ret &= arr[i];
+
+			sb.append(ret).append("\n");
+		}
+
+		br.close();
+		System.out.println(sb);
+	}
+
+	private static int[] nextIntArray(int N, BufferedReader br) throws Exception {
+		StringTokenizer st = new StringTokenizer(br.readLine().trim()," ");
+		int arr[] = new int[N];
+		for(int i = 0; i < N; i++)
+			arr[i] = Integer.parseInt(st.nextToken());
+		return arr;
+	}
+}
