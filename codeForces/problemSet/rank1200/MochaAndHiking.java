6be5cf8d60bc817c4e4551d1adb25b3763863bd9
Yadunandan Ahuja <yaduahuja24@gmail.com>
2021-08-23 15:18:19 +0530

    Added The Solutions According to their Ranks

--- codeForces/problemSet/rank1200/MochaAndHiking.java
+++ codeForces/problemSet/rank1200/MochaAndHiking.java
@@ -2,7 +2,7 @@
  *  Problem Link: https://codeforces.com/contest/1559/problem/C
  *  Runtime: 0.202s
  */
-package codeForces.round738;
+package codeForces.problemSet.rank1200;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
--- codeForces/problemSet/rank1400/MochaAndDianaEasy.java
+++ codeForces/problemSet/rank1400/MochaAndDianaEasy.java
@@ -4,7 +4,7 @@
  *
  *  Difference Between Hard and Easy Version is of Constraints
  */
-package codeForces.round738;
+package codeForces.problemSet.rank1400;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
